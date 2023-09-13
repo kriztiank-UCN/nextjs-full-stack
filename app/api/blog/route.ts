fcafd6a938bb8bc4280d3958729729a02b3057dc
kriztiank <kriztiank@gmail.com>
2023-09-13 18:43:32 +0200

    Creating A POST Handler

--- app/api/blog/route.ts
+++ app/api/blog/route.ts
@@ -1,28 +1,41 @@
-import prisma from "@/prisma";
-import { NextResponse } from "next/server";
+import prisma from '@/prisma'
+import { NextResponse } from 'next/server'
 
 export async function main() {
   try {
-    await prisma.$connect();
+    await prisma.$connect()
   } catch (err) {
-    return Error("Database Connection Unsuccessull");
+    return Error('Database Connection Unsuccessull')
   }
 }
 
 export const GET = async (req: Request, res: NextResponse) => {
-console.log("GET /api/blog");
-try {
-  await main();
-  const posts = await prisma.post.findMany();
-  return NextResponse.json({ message: "Success", posts }, { status: 200 });
-} catch (err) {
-  return NextResponse.json({ message: "Error", err }, { status: 500 });
-} finally {
-  await prisma.$disconnect();
+  // console.log('GET /api/blog')
+  try {
+    // connect to db with the main function
+    await main()
+    const posts = await prisma.post.findMany()
+    return NextResponse.json({ message: 'Success', posts }, { status: 200 })
+  } catch (err) {
+    return NextResponse.json({ message: 'Error', err }, { status: 500 })
+  } finally {
+    await prisma.$disconnect()
+  }
 }
 
-};
-
 export const POST = async (req: Request, res: NextResponse) => {
-  console.log("POST /api/blog");
-  };
+  // console.log('POST /api/blog')
+  try {
+    // destructuring the data from request.body
+    const { title, description } = await req.json()
+    // connect to db with the main function
+    await main()
+   // insert data to the database
+    const post = await prisma.post.create({ data: { description, title } })
+    return NextResponse.json({ message: 'Success', post }, { status: 201 })
+  } catch (err) {
+    return NextResponse.json({ message: 'Error', err }, { status: 500 })
+  } finally {
+    await prisma.$disconnect()
+  }
+}
