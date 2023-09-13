4fb325e7c996596c382c6bb7654b441f15e4ad10
kriztiank <kriztiank@gmail.com>
2023-09-13 19:26:32 +0200

    remove .env

---
+++ app/api/blog/[id]/route.ts
@@ -0,0 +1,7 @@
+import { NextResponse } from 'next/server'
+
+export const GET = async (req: Request, res: NextResponse) => {}
+
+export const Put = async (req: Request, res: NextResponse) => {}
+
+export const DELETE = async (req: Request, res: NextResponse) => {}
