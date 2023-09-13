d05e01efdb80f595ccf1d85cb68598efee1a82db
kriztiank <kriztiank@gmail.com>
2023-09-13 17:50:10 +0200

    Install Prisma and Setup With MongoDB

---
+++ prisma/index.ts
@@ -0,0 +1,41 @@
+// import { PrismaClient } from "@prisma/client";
+
+// let prisma: PrismaClient;
+// declare global {
+//   namespace NodeJS {
+//     interface Global {
+//       prisma: PrismaClient;
+//     }
+//   }
+// }
+
+// if (process.env.NODE_ENV === "production") {
+//   prisma = new PrismaClient();
+// } else {
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient();
+//   }
+//   prisma = global.prisma;
+// }
+
+// export default prisma;
+
+// https://www.traversymedia.com/blog/prisma-crash-course
+
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // Prisma Queries
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
