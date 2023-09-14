166e11d5bbe8418da976d12de35160d0dd4c29c9
kriztiank <kriztiank@gmail.com>
2023-09-14 12:29:43 +0200

    Create Dynamic API Route Handler With GET Request

--- app/api/blog/[id]/route.ts
+++ app/api/blog/[id]/route.ts
@@ -1,6 +1,28 @@
 import { NextResponse } from 'next/server'
+import { main } from '../route'
+import prisma from '@/prisma'
 
-export const GET = async (req: Request, res: NextResponse) => {}
+/* first we are getting the ID then we are connecting to the database then we are fetching the post by id and if we don't have the post we are sending the
+response message "Not Found" and if we have the post then we are sending the response message that this time we found a post with a message of "Success" */
+
+// Creating a Dynamic API Route Handler With GET Request
+export const GET = async (req: Request, res: NextResponse) => {
+  try {
+    // split the url to get the id from the second value in the array
+    const id = req.url.split('/blog/')[1]
+    // connect to db with the main function
+    await main()
+    // find the post with the id
+    const post = await prisma.post.findFirst({ where: { id } })
+    // validate if the post is found or not found
+    if (!post) return NextResponse.json({ message: 'Not Found' }, { status: 404 })
+    return NextResponse.json({ message: 'Success', post }, { status: 200 })
+  } catch (error) {
+    return NextResponse.json({ message: 'Error', error }, { status: 500 })
+  } finally {
+    await prisma.$disconnect()
+  }
+}
 
 export const Put = async (req: Request, res: NextResponse) => {}
 
