cd81bd6be8d8843d0eff6f17cd6d7cb545d0896c
kriztiank <kriztiank@gmail.com>
2023-09-14 14:50:18 +0200

    homepage design with Tailwind

---
+++ app/blog/add/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+// import { useRouter } from "next/navigation";
+// import { Fragment, useRef } from "react";
+// import { Toaster, toast } from "react-hot-toast";
+
+const AddBlog = () => {
+  return (
+    <div>AddBlog</div>
+  )
+}
+export default AddBlog
