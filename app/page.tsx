a0b6337bf9320e9c2d1fa3c881fafa237641fe0d
kriztiank <kriztiank@gmail.com>
2023-09-14 14:07:15 +0200

    Creating A Server Component With HTTP Request

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
+async function fetchBlogs() {
+  const res = await fetch('http://localhost:3000/api/blog', {
+    next: {
+      revalidate: 10,
+    },
+  })
+  const data = await res.json()
+  return data.posts
+}
 
-export default function Home() {
-  return (
-   <main>Hi</main>
-  )
+export default async function Home() {
+  const posts = await fetchBlogs()
+  console.log(posts)
+  return <main>Hi</main>
 }
