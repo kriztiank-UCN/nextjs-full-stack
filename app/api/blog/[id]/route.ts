fcf90741b3a8d37721f993d11468638a4e95bb30
kriztiank <kriztiank@gmail.com>
2023-09-14 13:02:06 +0200

    Creating A DELETE Handler

--- app/api/blog/[id]/route.ts
+++ app/api/blog/[id]/route.ts
@@ -12,7 +12,7 @@ export const GET = async (req: Request, res: NextResponse) => {
     const id = req.url.split('/blog/')[1]
     // connect to db with the main function
     await main()
-    // find the post with the id
+    // find the post by id
     const post = await prisma.post.findFirst({ where: { id } })
     // validate if the post is found or not found
     if (!post) return NextResponse.json({ message: 'Not Found' }, { status: 404 })
@@ -45,4 +45,19 @@ export const PUT = async (req: Request, res: NextResponse) => {
   }
 }
 
-export const DELETE = async (req: Request, res: NextResponse) => {}
+export const DELETE = async (req: Request, res: NextResponse) => {
+  try {
+    // split the url to get the id from the second value in the array
+    const id = req.url.split('/blog/')[1]
+
+    // connect to db with the main function
+    await main()
+    // delete the post by id
+    const post = await prisma.post.delete({ where: { id } })
+    return NextResponse.json({ message: 'Success', post }, { status: 200 })
+  } catch (error) {
+    return NextResponse.json({ message: 'Error', error }, { status: 500 })
+  } finally {
+    await prisma.$disconnect()
+  }
+}
